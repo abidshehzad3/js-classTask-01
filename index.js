50acacf7a8b606c4cd6d00d1af765fd98c4e1359
abidshehzad3 <mabidgulkhan@gmail.com>
2024-02-21 11:10:29 +0500

    done

---
+++ index.js
@@ -0,0 +1,57 @@
+// Welcome Message:
+
+// function displayWelcomeMessage() {
+//           var name = prompt("Please enter your name:");
+//          alert("Welcome, " + name + "!");
+//       }
+ 
+//  displayWelcomeMessage();
+
+
+// Quote Generator: 
+
+// let quotes = [
+//    "The only way to do great work is to love what you do. - Steve Jobs",
+//    "Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll",
+//    "Believe you can and you're halfway there. - Theodore Roosevelt",
+//    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+//    "Your time is limited, don't waste it living someone else's life. - Steve Jobs",
+//    "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt"
+// ];
+// function generateQuote() {
+
+//    var randomIndex = Math.floor(Math.random() * quotes.length);
+//    alert(quotes[randomIndex])
+// }
+
+// generateQuote();
+
+
+// Random Number Generator: 
+
+// function generateRandomNumber() {
+//    let randomNumber =Math.floor(Math.random() * 100);
+//    alert(randomNumber);
+
+// }
+// generateRandomNumber();
+
+
+
+
+
+
+
+
+
+
+//  let alphabet =[
+//    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 
+//    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+//    function getRandomAlphabet(){
+//       let randomNumber = Math.floor(Math.random() * alphabet.length)
+//       alert(alphabet[randomNumber])
+//    }
+
+//    getRandomAlphabet();
